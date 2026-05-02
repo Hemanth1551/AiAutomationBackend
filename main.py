3189edddb0a87ce90fd26ad8105eb052c8065ceb
Hemanth_ Dommeti <94308036+Hemanth1551@users.noreply.github.com>
2026-05-02 16:38:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"*",
+    allow_origin_regex=["http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://ai-automation-frontend-zeta.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
